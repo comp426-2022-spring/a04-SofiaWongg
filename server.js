d3b0d453541415477342c16234f45da38f569dab
Sofia Wong <ssofia@ad.unc.edu>
2022-04-10 18:50:25 -0400

    make stuff strings

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
     }
 
     const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    const info = stmt.run(String(logdata.remoteaddr), String(logdata.remoteuser), String(logdata.time), String(logdata.method, logdata.url), String(logdata.protocol), String(logdata.httpversion), String(logdata.status), String(logdata.referer), String(logdata.useragent))
     res.status(200).json(info);
     next();
 })
